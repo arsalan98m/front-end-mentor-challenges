6f50e5ff4ce6ea12cd1347bbd26b5b761e67dfb0
arsalan manzoor <arsalan1998m@gmail.com>
2020-12-31 01:22:32 +0500

    updated main index.html file and created app.js file and sepearted projects on array and then inserted on dom

---
+++ app.js
@@ -0,0 +1,45 @@
+const projects = [
+  {
+    title: "Four card feature section",
+    image: "./four-card-feature-section-master//design/desktop-design.jpg",
+    link: "./four-card-feature-section-master/index.html",
+  },
+
+  {
+    title: "sign up form",
+    image:
+      "./intro-component-with-signup-form-master/design/desktop-design.jpg",
+    link: "./intro-component-with-signup-form-master/index.html",
+  },
+
+  {
+    title: "Base apparel coming soon page",
+    image: "./base-apparel-coming-soon-master/design/desktop-design.jpg",
+    link: "./base-apparel-coming-soon-master/index.html",
+  },
+];
+
+const mainSection = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const projectData = projects
+    .map((project) => {
+      return `
+        <a
+          href="${project.link}"
+          target="_blank"
+          class="project"
+        >
+          <img
+            src="${project.image}"
+            alt=""
+          />
+          <footer>
+            <h4>${project.title}</h4>
+          </footer>
+        </a>
+        `;
+    })
+    .join("");
+  mainSection.innerHTML = projectData;
+});
